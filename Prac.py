ca56eb0c0e0f39a502718c3dc690c263a78134a4
Matias Pavez <kiorrvboy@gmail.com>
2025-06-26 15:46:49 -0400

    K s t

--- Prac.py
+++ Prac.py
@@ -1,25 +1,34 @@
 from funciones import menu
 
 pokemons = []
+tipos_validos = ["fuego","agua", "psiquico", "planta", "lucha", "veneno", "electrico", 
+                 "roca", "dragon", "bicho", "tierra", "volador", "hielo", "fantasma"]
 
 print("**** Bienvenido ****")
 while True:
     menu()
     opcionI = int(input("Ingrese una opción: "))
     if opcionI == 1: 
-        nombrePokemon = input("Ingrese el nombre del pokemon: ")
+        nombrePokemon = input("Ingrese el nombre del pokemon: ").lower()
         if nombrePokemon in pokemons:  
             print("El pokemon ingresado ya esta registrado.")
             continue
-        else:
-            pokemons.append(nombrePokemon)
-        tipoPokemonn = input("Ingese de que tipo es el pokemon: ")
-        if tipoPokemonn != "fuego":
-            pass
+        tipoPokemon = input("Ingese de que tipo es el pokemon: ").lower()
+        if tipoPokemon not in tipos_validos:
+            print("Tipo invalido.")
+            continue
+        pokemons.append({
+            "nombre": nombrePokemon,
+            "tipo": tipoPokemon})
     elif opcionI == 2:
-        for buscar in pokemons:
-            buscar_pokemon = input("Ingrese el nombre del pokemon a buscar: ")
-            if buscar_pokemon in pokemons:
-                print(pokemons)
-            else:
-                print("El pokemon no se encuentra registrado.")
+        buscar_pokemon = input("Ingrese el nombre del pokemon a buscar: ").lower()
+        encontrado= False
+        for i in pokemons:
+            if i["nombre"] == buscar_pokemon:
+                print("***Pokemon encontrado***\n")
+                print(f"nombre: {i["nombre"]}")
+                print(f"tipo: {i["tipo"]}\n")
+                encontrado = True
+                break
+        if not encontrado:
+            print("\nNo se ha encontrado al pokemon.")
